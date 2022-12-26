4badadec9a87a3f13aa77346c4cf51e44924eff9
Jay-1232 <114244101+Jay-1232@users.noreply.github.com>
2022-12-26 19:02:53 +0530

    index

---
+++ program1.cpp
@@ -0,0 +1,15 @@
+#include <iostream>
+#include <stdlib.h>
+
+using namespace std;
+
+int main()
+{
+    int a,i;
+    cout << "Enter a:"<< endl;
+    cin >> a;
+    for (i=0; i<=a; i++){
+        cout << i;
+    }
+    return 0;
+}
